f874f0ca7513b51e4b79246b908185d8c9cc4a83
Watkins-Brown <76839@psdschools.org>
2019-02-01 14:35:57 -0700

    kinda workable now

---
+++ html/login.js
@@ -0,0 +1,23 @@
+/*sendPost = function() {
+  //console.log('sent');
+  $.post('/post', {value:'123'}, function(data, status){
+    //console.log(data+' : status is '+status);
+  });
+}
+*/
+
+function getUrlVars(str){ /* Stole from Ryan b/c i'm lazy, credit to him */
+  var vars = [], hash;
+  var hashes = str.slice(str.indexOf('?') + 1).split('&');
+  for(var i = 0; i < hashes.length; i++){
+    hash = hashes[i].split('=');
+    if(vars.indexOf(hash[0])>-1){
+      vars[hash[0]]+=","+hash[1];
+    }
+    else{
+      vars.push(hash[0]);
+      vars[hash[0]] = hash[1];
+    }
+  }
+  return vars;
+}
--- server.js
+++ server.js
@@ -3,3 +3,30 @@ var app = express();
 var http = require('http').Server(app);
 var url = require('url');
 var fs = require('fs');
+var bodyParser = require('body-parser');
+
+app.use(express.static(__dirname + '/html'));
+app.use(bodyParser.json());
+
+app.use(bodyParser.urlencoded({
+     extended: true
+}));
+
+// GET method route
+app.get('/', function (req, res) {
+  res.sendFile('index.html');
+})
+
+// POST method route
+app.post('/', function (req, res) {
+  //console.log(req.body);
+  res.send('POST request to the homepage');
+})
+
+http.listen(8080, function () {
+  console.log(`Listening on port ${http.address().port}`);
+});
+
+app.all('/secret', function (req, res) {
+  res.send('a');
+})
