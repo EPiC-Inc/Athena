ea6f116a4c95e20d135a204ed7cfa1a978ea1999
Watkins-Brown <76839@psdschools.org>
2019-02-04 11:24:35 -0700

    more or less working

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ var url = require('url');
 var fs = require('fs');
 var bodyParser = require('body-parser');
 
+const port = 8080;
+
 app.use(express.static(__dirname + '/html'));
 app.use(bodyParser.json());
 
@@ -13,12 +15,13 @@ app.use(bodyParser.urlencoded({
 }));
 
 // GET method route
-app.get('/', function (req, res) {
+app.get('/', function(req, res) {
+  var page = 'INDEX';
   res.sendFile('index.html');
 });
 
-// POST method route
-app.post('/', function (req, res) {
+// POST method routing
+app.post('/', function(req, res) {
   //console.log(req.body);
   //parse the data sent
   var userData = req.body;
@@ -26,10 +29,16 @@ app.post('/', function (req, res) {
   res.send('POST request to the homepage');
 });
 
-http.listen(8080, function () {
-  console.log(`Listening on port ${http.address().port}`);
+app.post('/database-entry', function(req, res){
+  //
 });
 
+// ALL routing
 app.all('/secret', function (req, res) {
   res.send('a');
-})
+});
+
+// Server starting
+http.listen(port, function () {
+  console.log(`Listening on port ${http.address().port}`);
+});
