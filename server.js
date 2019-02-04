2c4bc933466475660f4bfe3089961217080fc9d0
Watkins-Brown <76839@psdschools.org>
2019-02-04 11:26:52 -0700

    setting up heroku

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var url = require('url');
 var fs = require('fs');
 var bodyParser = require('body-parser');
 
-const port = 8080;
+const port = process.env.PORT || 80;;
 
 app.use(express.static(__dirname + '/html'));
 app.use(bodyParser.json());
