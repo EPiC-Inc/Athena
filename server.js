e432ab893617e2168335e66b41ed83f28a56aa4e
Watkins-Brown <76839@psdschools.org>
2019-02-05 13:47:48 -0700

    a

--- server.js
+++ server.js
@@ -1,22 +1,22 @@
-var express = require('express');
+var express = require("express");
 var app = express();
-var http = require('http').Server(app);
-var url = require('url');
-var fs = require('fs');
-var bodyParser = require('body-parser');
-const { Client } = require('pg'); // For the database
+var http = require("http").Server(app);
+var url = require("url");
+var fs = require("fs");
+var bodyParser = require("body-parser");
+const { Client } = require("pg"); // For the database
 
 const port = process.env.PORT || 8080;
 
 // To connect to database:
 var client = new Client({
   connectionString: process.env.DATABASE_URL,
-  ssl: true,
+  ssl: true
 });
 client.connect();
 //console.log(process.env.DATABASE_URL);
 
-app.use(express.static(__dirname + '/html'));
+app.use(express.static(__dirname + "/html"));
 app.use(bodyParser.json());
 
 app.use(bodyParser.urlencoded({
@@ -30,14 +30,14 @@ async function querydb(command, req, res) {
   client.query(command)
     .then((dbres) => {console.log(dbres.rows);
       res.send(dbres.rows)})
-    .catch(e => console.log(e.stack), res.send(e.stack))
+    .catch((e) => console.log(e.stack), res.send(e.stack))
 }
 
 function async_dbquery(req, res) {
   //console.log("Result of "+req.body.command+" : "+qdb(req.body.command));
   return new Promise(function(resolve, reject){
     // Do async
-    //console.log('Promise finished:'+querydb(req.body.command));
+    //console.log("Promise finished:"+querydb(req.body.command));
     //dbres = result;
     resolve(querydb(req.body.command, req, res));
     //resolve(dbres);
@@ -46,40 +46,40 @@ function async_dbquery(req, res) {
 }
 
 // GET method route
-app.get('/', function(req, res){
-  res.sendFile('index.html');
+app.get("/", function(req, res){
+  res.sendFile("index.html");
 });
 
 // POST method routing
-app.post('/', function(req, res){
+app.post("/", function(req, res){
   //console.log(req.body);
   //parse the data sent
   var userData = req.body;
   console.log(userData);
-  res.send('POST request to the homepage');
+  res.send("POST request to the homepage");
 });
 
-app.post('/database-entry', function(req, res){
-  res.send('wip');
+app.post("/database-entry", function(req, res){
+  res.send("wip");
 });
 
-app.post('/database-query', function(req, res){
+app.post("/database-query", function(req, res){
   //console.log(querydb(req.body.command));
   async_dbquery(req, res)
   .then((result) => {
-    res.send(result);
+    //res.send(result);
     console.log("Promise verified:"+result);
   })
   .catch((err) => {
-    console.log('Stuff broke');
-    res.send('500 Stuff Broke<br>If you\'re the server owner, see the logs for more info');
+    console.log("Stuff broke");
+    res.send("500 Stuff Broke<br>If you're the server owner, see the logs for more info");
   });
-  //res.send('wip');
+  //res.send("wip");
 });
 
 // ALL routing
-app.all('/secret', function (req, res) {
-  res.send('a');
+app.all("/secret", function (req, res) {
+  res.send("a");
 });
 
 // Server starting
@@ -87,4 +87,4 @@ http.listen(port, function () {
   console.log(`Listening on port ${http.address().port}`);
 });
 
-//console.log(querydb('select * from users'));
+//console.log(querydb("select * from users"));
