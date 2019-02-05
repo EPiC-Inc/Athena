247a0da800eedb85c6b5ce036f516902c42cc310
Watkins-Brown <76839@psdschools.org>
2019-02-05 14:43:55 -0700

    a

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ async function querydb(command, req, res) {
   client.query(command)
     .then((dbres) => {console.log(dbres.rows);
       res.send(dbres.rows)})
-    .catch((err) => console.log(err.stack), res.send(err.stack))
+    .catch((err) => {console.log(err.stack), res.send(err.stack)})
 }
 
 function async_dbquery(req, res) {
